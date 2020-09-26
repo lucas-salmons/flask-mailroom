d4fd90bb46543a82591f168f3607d551c045fcc5
Lucas Salmons <lucsalmons@gmail.com>
2020-09-25 19:53:31 -0700

    added template for add.jinja2 displays form on GET

--- main.py
+++ main.py
@@ -3,21 +3,32 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return redirect(url_for('all'))
 
+
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/add')
+def add():
+    if request.method == 'POST':
+        # add the donation
+        # redirect to home/all
+        pass
+
+    return render_template('add.jinja2')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
